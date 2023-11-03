86c139ad0064853d85c4cb08181467e223e876eb
ARS <lssibb@yandex.ru>
2023-11-04 00:33:38 +0700

    дшна

--- Assets/Scripts/Player_move.cs
+++ Assets/Scripts/Player_move.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class Player_move : MonoBehaviour
 {
     private Rigidbody rb;
-    float n = 0;
-    float b = 0;
-    float m = 0;
+   
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,13 +16,7 @@ public class Player_move : MonoBehaviour
         float y=Input.GetAxisRaw("Vertical");
         // float z=Input.GetAxisRaw("z");
         float z = 0;
-        transform.localScale = new Vector3(b, n, m);
-        if (z == 0)
-        {
-            n++;
-            b++;
-            m++;
-        }
+
         if (x!=0 || y!=0 || z != 0)
         {
             rb.AddForce (new Vector3(x,0,y),ForceMode.Impulse);
